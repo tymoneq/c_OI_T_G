80044b0034598b2c1fadc6db7548fc66a709e338
TyMooNeEQ <tymontumialis@gmail.com>
2022-09-26 20:36:48 +0200

    początek z omównia

--- cukiernia_by_Tymoneq/cuk.cpp
+++ cukiernia_by_Tymoneq/cuk.cpp
@@ -2,93 +2,21 @@
 
 using namespace std;
 
-char max(unsigned int &droz, unsigned int &pon, unsigned int &rog)
-{
-    unsigned int max = 0;
-    char type;
-
-    if (droz == pon == rog)
-    {
-        type = 'W';
-        return type;
-    }
-
-    if (max < droz)
-    {
-        max = droz;
-        type = 'D';
-    }
-
-    if (pon > max)
-    {
-        max = pon;
-        type = 'P';
-    }
-    if (rog > max)
-    {
-        max = rog;
-        type = 'R';
-    }
-    // if (droz == pon)
-    // {
-    //     type = 'N';
-    // }
-    // if (pon == rog)
-    // {
-    //     type = 'M';
-    // }
-    // if (droz == rog)
-    // {
-    //     type = 'B';
-    // }
-
-    return type;
-}
-
 int main()
 {
     int n;
-    unsigned int wynik = 0;
+    unsigned int wynik = 0, koszt_pt = 0, droz, rog, pon, suma = 0;
     cin >> n;
 
     for (int i = 0; i < n; i++)
     {
-        unsigned int droz = 0, pon = 0, rog = 0;
-        char type;
+        suma = 0;
         cin >> droz >> pon >> rog;
-        type = max(droz, pon, rog);
-        if (type == 'D')
-        {
-            wynik += pon + rog;
-        }
-        else if (type == 'P')
-        {
-            wynik += droz + rog;
-        }
-        else if (type == 'R')
-        {
-            wynik += droz + pon;
-        }
-        else if (type == 'W')
-        {
-            wynik += droz + pon;
-        }
-        else if (type == 'N')
-        {
-            wynik += pon + rog;
-        }
-        else if (type == 'M')
-        {
-            wynik += droz + rog;
-        }
-        else if (type == 'B')
-        {
-            wynik += rog + pon;
-        }
+        suma = droz + pon + rog;
+        koszt_pt += suma - max(max(droz,pon), rog);
         
-        cout << type << "\n";
     }
-    cout << wynik << "\n";
+    cout << koszt_pt << "\n";
 
     return 0;
 }
