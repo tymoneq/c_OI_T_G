9e3b6c4e56317baf372ab81fd0266d243e5cecef
tymoneq <tymontumialis@gmail.com>
2022-09-28 09:05:39 +0200

    liczby

--- licznik_dl_by_Tymoneq/licz.cpp
+++ licznik_dl_by_Tymoneq/licz.cpp
@@ -1,112 +1,148 @@
 #include <bits/stdc++.h>
-#include <limits>
 
 using namespace std;
 
 int main()
 {
-    unsigned int n, z, number;
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string dl_wew{}, dl_zew{};
+    string operacja{};
+    char y{};
+    unsigned int n{}, z{};
+    set<unsigned int> more_than_10;
+    set<unsigned int> less_than_8;
+
     cin >> n >> z;
+    cin >> dl_wew >> dl_zew;
 
-    unsigned int *dl_wew = new unsigned int[n - 1]();
-    unsigned int *dl_zew = new unsigned int[n - 1]();
-    unsigned int *suma_dl = new unsigned int[n - 1]();
-
-    cin >> number;
-
-    for (int i = n - 1; i > 0; i--)
+    for (int i = 0; i < n - 1; i++)
     {
-        dl_wew[i] = number % 10;
-        number /= 10;
+        unsigned int suma = 0;
+        unsigned int temp_1 = dl_wew[i] - 48;
+        unsigned int temp_2 = dl_zew[i] - 48;
+
+        suma = temp_1 + temp_2;
+        if (suma <= 8)
+        {
+            less_than_8.insert(n - 1 - i);
+        }
+        else if (suma >= 10)
+        {
+            more_than_10.insert(n - 1 - i);
+        }
     }
 
-    cin >> number;
-    // std::cin.clear();
-    // std::cin.ignore( std::numeric_limits < std::streamsize >::max(), '\n' );
-
-    for (int i = n - 1; i > 0; i--)
-    {
-        dl_zew[i] = number % 10;
-        number /= 10;
-        suma_dl[i] = dl_wew[i] + dl_zew[i];
-    }
     for (int i = 0; i < z; i++)
     {
+        int x{};
 
-        char operacja{};
-        int x{0}, y{0};
         cin >> operacja >> x;
-
-        if (operacja == 'W')
+        if (operacja == "W")
         {
             cin >> y;
 
-            dl_wew[n - x] = y;
-            suma_dl[n - x] = dl_wew[n - x] + dl_zew[n - x];
+            if (dl_wew[n - 1 - x] - 48 + dl_zew[n - 1 - x] - 48 >= 10)
+                more_than_10.erase(x);
+
+            else if (dl_wew[n - 1 - x] - 48 + dl_zew[n - 1 - x] - 48 <= 8)
+                less_than_8.erase(x);
+
+            dl_wew[n - 1 - x] = y;
+
+            if (y - 48 + dl_zew[n - 1 - x] - 48 >= 10)
+                more_than_10.insert(x);
+
+            else if (y - 48 + dl_zew[n - 1 - x] - 48 <= 8)
+                less_than_8.insert(x);
+
+            for (auto el : more_than_10)
+            {
+                cout << "more than 10 " << el << " ";
+            }
+            cout << "\n";
+            for (auto el : less_than_8)
+            {
+                cout << "less than 8 " << el << " ";
+            }
+            cout << "\n";
+            cout << "\n";
         }
-        else if (operacja == 'Z')
+        else if (operacja == "Z")
         {
             cin >> y;
 
-            dl_zew[n - x] = y;
-            suma_dl[n - x] = dl_wew[n - x] + dl_zew[n - x];
+            if (dl_wew[n - 1 - x] - 48 + dl_zew[n - 1 - x] - 48 >= 10)
+                more_than_10.erase(x);
+
+            else if (dl_wew[n - 1 - x] - 48 + dl_zew[n - 1 - x] - 48 <= 8)
+                less_than_8.erase(x);
+
+            dl_zew[n - 1 - x] = y;
+
+            if (dl_wew[n - 1 - x] - 48 + y - 48 >= 10)
+                more_than_10.insert(x);
+
+            else if (dl_wew[n - 1 - x] - 48 + y - 48 <= 8)
+                less_than_8.insert(x);
+
+            for (auto el : more_than_10)
+            {
+                cout << "more than 10 " << el << " ";
+            }
+            cout << "\n";
+            for (auto el : less_than_8)
+            {
+                cout << "less than 8 " << el << " ";
+            }
+            cout << "\n";
+            cout << "\n";
         }
-        else if (operacja == 'S')
+        else if (operacja == "S")
         {
+            unsigned int suma{0};
+            unsigned int temp_dl_wew = dl_wew[n - 1 - x] - 48;
+            unsigned int temp_dl_zew = dl_zew[n - 1 - x] - 48;
+            unsigned int index_more_than10 = 0;
+            unsigned int index_less_than8 = 0;
+
             if (n - x == n - 1)
             {
-                cout << suma_dl[n - x] % 10 << "\n";
+
+                suma = temp_dl_wew + temp_dl_zew;
+                cout << suma % 10 << "\n";
             }
             else
             {
-                if (suma_dl[n - x + 1] <= 8)
+                for (auto itr = more_than_10.rbegin(); itr != more_than_10.rend(); itr++)
                 {
-
-                    cout << suma_dl[n - x] % 10 << "\n";
-                }
-                else if (suma_dl[n - x + 1] >= 10)
-                {
-
-                    cout << (suma_dl[n - x] + 1) % 10 << "\n";
-                }
-
-                else if (suma_dl[n - x + 1] == 9)
-                {
-                    bool test = false;
-                    for (int j = n - x + 1; j < n; j++)
+                    if (*itr < x)
                     {
-                        if (suma_dl[j] <= 8)
-                        {
-                            cout << suma_dl[n - x] % 10 << "\n";
-                            test = true;
-                            break;
-                        }
-                        else if (suma_dl[j] >= 10)
-                        {
-
-                            cout << (suma_dl[n - x] + 1) % 10 << "\n";
-                            test = true;
-                            break;
-                        }
-                        else
-                        {
-
-                            continue;
-                        }
-                    }
-                    if (!test)
-                    {
-
-                        cout << suma_dl[n - x] % 10 << "\n";
+                        return index_more_than10 = *itr;
                     }
                 }
+                for (auto itr = less_than_8.rbegin(); itr != less_than_8.rend(); itr++)
+                {
+                    if (*itr < x)
+                    {
+                        return index_less_than8 = *itr;
+                    }
+                }
+                if (index_more_than10 > index_less_than8)
+                {
+                    suma = temp_dl_wew + temp_dl_zew + 1;
+                }
+                else if (index_more_than10 < index_less_than8)
+                {
+                    suma = temp_dl_wew + temp_dl_zew;
+                }
+
+                cout << suma % 10 << '\n';
             }
         }
     }
-
-    delete[] suma_dl;
-    delete[] dl_wew;
-    delete[] dl_zew;
 
     return 0;
 }
