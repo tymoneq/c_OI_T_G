e49621ce0b1be7df951f4a1dfb340b094b210297
TyMooNeEQ <tymontumialis@gmail.com>
2022-09-30 22:06:40 +0200

    piekarnia

--- cukiernia_by_Tymoneq/cuk.cpp
+++ cukiernia_by_Tymoneq/cuk.cpp
@@ -1,30 +1,80 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+bool sortcol(const vector<int> &v1, const vector<int> &v2)
+{
+    return v1[1] > v2[1];
+}
 
+bool sortcol2(const vector<int> &v1, const vector<int> &v2)
+{
+    return v1[2] > v2[2];
+}
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     int n;
-    unsigned int wynik = 0, koszt_pt = 0, droz, rog, pon, suma = 0;
+    unsigned int wynik = 0, koszt_pt = 0, droz, rog, pon;
     cin >> n;
     vector<vector<int>> v(n);
 
     for (int i = 0; i < n; i++)
     {
-        suma = 0;
+
         cin >> droz >> pon >> rog;
-        suma = droz + pon + rog;
-        koszt_pt += suma - max(max(droz, pon), rog);
+        koszt_pt += droz + pon + rog;
 
         v[i].push_back(droz);
         v[i].push_back(pon);
         v[i].push_back(rog);
     }
-    cout << koszt_pt << "\n";
+    // first column sort
+    make_heap(v.begin(), v.end());
     for (auto el : v)
     {
         cout << el[0] << " " << el[1] << " " << el[2] << "\n";
-    }
+        if (el[0] > el[1] + el[2])
+            koszt_pt -= el[0];
 
+        else
+        {
+
+            break;
+        }
+    }
+    cout << endl;
+    // second column sort
+    sort(v.begin(), v.end(), sortcol);
+    for (auto el : v)
+    {
+        cout << el[0] << " " << el[1] << " " << el[2] << "\n";
+        if (el[1] >= el[0] + el[2])
+            koszt_pt -= el[1];
+
+        else
+        {
+
+            break;
+        }
+    }
+    cout << endl;
+    // third column sort
+
+    sort(v.begin(), v.end(), sortcol2);
+    for (auto el : v)
+    {
+        cout << el[0] << " " << el[1] << " " << el[2] << "\n";
+        if (el[2] >= el[0] + el[1])
+            koszt_pt -= el[2];
+
+        else
+        {
+
+            break;
+        }
+    }
+    cout << koszt_pt << "\n";
     return 0;
 }
