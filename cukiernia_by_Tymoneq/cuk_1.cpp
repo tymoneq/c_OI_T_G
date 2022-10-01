999cf22bbefd1a04c538dc3ea9cedda707d9bed3
TyMooNeEQ <tymontumialis@gmail.com>
2022-10-01 15:01:46 +0200

    cuk_1

---
+++ cukiernia_by_Tymoneq/cuk_1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    unsigned int wynik = 0,  droz, rog, pon, suma = 0;
+    unsigned long long koszt_pt = 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        suma = 0;
+        cin >> droz >> pon >> rog;
+        suma = droz + pon + rog;
+        koszt_pt += suma - max(max(droz,pon), rog);
+        
+    }
+    cout << koszt_pt << "\n";
+
+    return 0;
+}
