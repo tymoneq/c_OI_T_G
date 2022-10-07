3a063e79f20971c3c571c7c06a2a6fd48e800708
TyMooNeEQ <tymontumialis@gmail.com>
2022-10-07 20:09:26 +0200

    oj

--- Gang_biciaków_by_Tymoneq/main.cpp
+++ Gang_biciaków_by_Tymoneq/main.cpp
@@ -9,15 +9,37 @@ int main()
     cin >> n >> m >> z;
     vector<vector<pair<int, int>>> G(n);
     vector<set<int>> set_table;
-
-    for (size_t i = 0; i < n; i++)
+    map<int, pair<int, int>> street_num;
+    for (size_t i = 1; i < n; i++)
     {
         int a, b, c;
         cin >> a >> b >> c;
-        auto k1 = make_pair(b, c);
-        auto k2 = make_pair(a, c);
-        G[a].push_back(k1);
-        G[b].push_back(k2);
+        auto pair_1 = make_pair(b, c);
+        auto pair_2 = make_pair(a, c);
+        G[a].push_back(pair_1);
+        G[b].push_back(pair_2);
+        street_num.insert({i, make_pair(a, b)});
+    }
+    for (int i = 0; i < z; i++)
+    {
+        char oper;
+        int city;
+        cin >> oper >> city;
+        if (oper == 'Z')
+        {
+            // dijkstra
+        }
+        else if (oper == 'B')
+        {
+            int biciak{};
+            cin >> biciak;
+            pair<int, int> it = street_num.at(city);
+            int city_a = it.first;
+            int city_b = it.second;
+            auto itr1 = find(G.begin(), G.end(),city_a);
+           
+            
+        }
     }
 
     return 0;
