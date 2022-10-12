e724de037e4ec46aacfa01eefef907de645b9c65
gaweljablonski <gawel.jablonski2006@gmail.com>
2022-10-12 07:41:16 +0200

    JD++ START

---
+++ JD++/podOlimpiade.cpp
@@ -0,0 +1,47 @@
+#include<pod/olimpiade.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "'Pod Olimpiade' - JD\n";
+
+    sito(10);
+
+    return 0;
+}
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void vectorIntPrint(vector<int> &vector)
+{
+    if (!vector.size())
+        return;
+    for (int element : vector)
+        cout << element << " ";
+    cout << endl;
+}
+
+bool tab[100000000];
+
+void sito(int const &n)
+{
+    // int n = 10000;
+    // cin >> n;
+    vector<int> se;
+
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (tab[i] == 0)
+            for (int j = i * i; j <= n; j += i)
+                tab[j] = true;
+    }
+
+    for (int i = 2; i <= n; i++)
+        if (tab[i] == 0)
+            se.push_back(i);
+    vectorIntPrint(se);
+}
+*/
